faba410167040d1dfd642698f09a1319d4768fb1
durand_u <durand_u@epitech.eu>
2015-02-19 11:11:19 +0100

    2$

--- philo.c
+++ philo.c
@@ -5,8 +5,11 @@
 ** Login   <durand_u@epitech.net>
 ** 
 ** Started on  Thu Feb 19 10:57:04 2015 Rémi DURAND
-** Last update Thu Feb 19 10:58:56 2015 Rémi DURAND
+** Last update Thu Feb 19 11:09:20 2015 Rémi DURAND
 */
+
+#include <pthread.h>
+#include "philo.h"
 
 int	main()
 {
---
+++ philo.h
@@ -0,0 +1,16 @@
+/*
+** philo.h for philo in /home/durand_u/github/PSU_2014_philo
+** 
+** Made by Rémi DURAND
+** Login   <durand_u@epitech.net>
+** 
+** Started on  Thu Feb 19 11:09:26 2015 Rémi DURAND
+** Last update Thu Feb 19 11:10:34 2015 Rémi DURAND
+*/
+
+#ifndef _PHI_H_
+# define _PHI_H_
+
+
+
+#endif /* !_PHI_H_ */
